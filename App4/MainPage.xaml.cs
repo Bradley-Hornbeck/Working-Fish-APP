609c7432186e49b3b3c7f760514ae5b096506d83
Bradley Hornbeck <Bradley.Hornbeck@outlook.com>
2017-10-30 14:47:00 -0400

    Add project files.

---
+++ App4/MainPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace App4
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        BlankPage1 RF = new BlankPage1();
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void RedFish_click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(BlankPage1));
+        }
+
+        private void Snook_click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Trout_click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Mackerel_click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
